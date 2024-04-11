b4d2959061790fa75855fe138c6fb9e3d2394e36
Hayk <14haiko16op@gmail.com>
2024-04-11 22:18:47 +0400

    news

--- app/src/main/java/com/example/igifted/LoginActivity.java
+++ app/src/main/java/com/example/igifted/LoginActivity.java
@@ -1,20 +1,94 @@
 package com.example.igifted;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
 public class LoginActivity extends AppCompatActivity {
-    TextView createAccountBtnTextView;
+
+    EditText loginEmail, loginPassword;
+    Button loginBtn;
+    TextView registerTVB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        createAccountBtnTextView = findViewById(R.id.register_tvb);
+        loginEmail = findViewById(R.id.email_login);
+        loginPassword = findViewById(R.id.password_login);
+        registerTVB = findViewById(R.id.register_tvb);
+        loginBtn = findViewById(R.id.login_btn);
+    }
 
-        createAccountBtnTextView.setOnClickListener((v)-> startActivity(new Intent(LoginActivity.this, RegActivity.class)));
+    public Boolean validateEmail(){
+        String val = loginEmail.getText().toString();
+        if(val.isEmpty()){
+            loginEmail.setError("Email Can't be empty");
+            return false;
+        }else{
+            loginEmail.setError(null);
+            return true;
+        }
+    }
+
+    public Boolean validatePassword(){
+        String val = loginPassword.getText().toString();
+        if(val.isEmpty()){
+            loginPassword.setError("Password Can't be empty");
+            return false;
+        }else{
+            loginPassword.setError(null);
+            return true;
+        }
+    }
+
+    public void checkUser(){
+        String userEmail = loginEmail.getText().toString().trim();
+        String userPassword = loginPassword.getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkUserDatabase = reference.orderByChild("email").equalTo(userEmail);
+
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    loginEmail.setError(null);
+                    String passwordFromDB = snapshot.child(userEmail).child("password").getValue(String.class);
+
+                    if(!Objects.equals(passwordFromDB,userPassword)){
+                        loginEmail.setError(null);
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }else{
+                        loginPassword.setError("Invalid Password");
+                        loginPassword.requestFocus();
+                    }
+                }else{
+                    loginEmail.setError("User does not exist");
+                    loginEmail.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/igifted/RegActivity.java
+++ app/src/main/java/com/example/igifted/RegActivity.java
@@ -4,18 +4,66 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class RegActivity extends AppCompatActivity {
-    TextView loginBtnTextView;
 
-
+    EditText regUsername, regEmail, regPassword, regConfPassword;
+    TextView loginTVB;
+    Button regBtn;
+    FirebaseDatabase database;
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
 
-        loginBtnTextView = findViewById(R.id.login_tvb);
+        regUsername = findViewById(R.id.username);
+        regEmail = findViewById(R.id.email_reg);
+        regPassword = findViewById(R.id.password_reg);
+        regConfPassword = findViewById(R.id.confirm_password);
+        regBtn = findViewById(R.id.register_btn);
+        loginTVB = findViewById(R.id.login_tvb);
 
-        loginBtnTextView.setOnClickListener((v)-> startActivity(new Intent(RegActivity.this, LoginActivity.class)));
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                database = FirebaseDatabase.getInstance();
+                reference = database.getReference("users");
+
+                String username = regUsername.getText().toString();
+                String email = regEmail.getText().toString();
+                String password = regPassword.getText().toString();
+                String conf_password = regConfPassword.getText().toString();
+
+                if(password == conf_password) {
+                    HelperClass helperClass = new HelperClass(username, email, password);
+                    reference.child(username).setValue(helperClass);
+
+
+                    Toast.makeText(RegActivity.this, "You have registered successfully!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(RegActivity.this, "Passwords don't match!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        loginTVB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
