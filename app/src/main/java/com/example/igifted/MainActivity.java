92f16c33e4305be81bcd590483b6494448d8d4de
Hayk <14haiko16op@gmail.com>
2024-04-13 00:15:48 +0400

    bugs fixed

--- app/src/main/java/com/example/igifted/MainActivity.java
+++ app/src/main/java/com/example/igifted/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.igifted;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.igifted.databinding.ActivityMainBinding;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,8 +61,6 @@ public class MainActivity extends AppCompatActivity {
                 replaceFragment(new HomeFragment());
             }else if(item.getItemId() == R.id.store){
                 replaceFragment(new StoreFragment());
-            }else if(item.getItemId() == R.id.add){
-                replaceFragment(new AddFragment());
             }else if(item.getItemId() == R.id.cart){
                 replaceFragment(new CartFragment());
             }else if(item.getItemId() == R.id.profile){
@@ -37,6 +69,10 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         });
+    }
+    public void toUpload(View view) {
+        Intent intent = new Intent(MainActivity.this, UploadActivity.class);
+        startActivity(intent);
     }
 
     private void replaceFragment(Fragment fragment){
