2fb8c872340ae56ca6842b6e23a9a052e277baef
Hayk <14haiko16op@gmail.com>
2024-04-12 23:15:51 +0400

    omit

--- app/src/main/java/com/example/igifted/MainActivity.java
+++ app/src/main/java/com/example/igifted/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.igifted;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.igifted.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.home){
+                replaceFragment(new HomeFragment());
+            }else if(item.getItemId() == R.id.store){
+                replaceFragment(new StoreFragment());
+            }else if(item.getItemId() == R.id.add){
+                replaceFragment(new AddFragment());
+            }else if(item.getItemId() == R.id.cart){
+                replaceFragment(new CartFragment());
+            }else if(item.getItemId() == R.id.profile){
+                replaceFragment(new ProfileFragment());
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
